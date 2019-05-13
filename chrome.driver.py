00d3f67a1af45566f006c47237e0c6e1b7b3d327
praveen25kumar <50565544+praveen25kumar@users.noreply.github.com>
2019-05-13 12:12:28 +0530

    Update chrome.driver.py

--- chrome.driver.py
+++ chrome.driver.py
@@ -2,11 +2,7 @@ from selenium import webdriver
 
 chrome='C:/Users/mobiledeveloper/Downloads/New folder/chromedriver'
 a=input("enter ur dot com")
-#b=str(a)
 browser = webdriver.Chrome(chrome)
 browser.get('http://'+a+':8069/')
 
 
-#https://www.google.com/search?source=hp&ei=-QrRXNmICdPZz7sPq42aoAc&q=
-
-#http://192.168.20.151:8069/
